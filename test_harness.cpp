6d2727d07902f74716b5675835d3e612c3a95b32
Max Darling <darlingm@stanford.edu>
2019-12-03 15:53:41 -0800

    -added generateKeys() method which generates dense/sparse data, (almost) exactly like main.cpp

--- test_harness.cpp
+++ test_harness.cpp
@@ -1,7 +1,10 @@
 #include "adaptive_hash.hpp"
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
+using std::vector;
+using std::string;
 
 //basic test to ensure nothing is broken
 void create_simple_hash_table() { //assumes all input is valid
@@ -14,11 +17,44 @@ void create_simple_hash_table() { //assumes all input is valid
     
 }
 
+//given an empty vector (already allocated), insert appropriate keys
+void generateKeys(vector<uint64_t>& keys, size_t size, string type = "DENSE") {
+    std::mt19937 gen(197); //random func with seed as 197.
+    
+    if (type == "DENSE") {
+        for (int i = 0; i<size; i++) {
+            keys[i] = i+1; //b/c 0 isnt valid.
+        }
+        
+        std::shuffle(keys.begin(), keys.end(), gen);
+    }
+    //todo: generateIndexData() in main.cpp ensures no dupes
+    else if (type == "SPARSE") {
+        uint64_t max = uint64_t(pow(2,64) - 1);
+        std::uniform_int_distribution<uint64_t> dis(1, max);
+        for (int i = 0; i<size; i++) {
+            keys[i] = dis(gen);
+        }
+    }
+    else {
+        throw "error - invalid distribution type argument";
+    }
+}
+
+//test to see if generateKeys() works. It does!
+void experiment1() {
+    vector<uint64_t> vec (16);
+    generateKeys(vec, vec.size(), "SPARSE");
+    for (auto it = vec.begin(); it!=vec.end(); it++)
+        std::cout<<*it<<std::endl;
+}
 
 
 int main(int argc, char*argv[]) {
     
-    create_simple_hash_table();
+    //create_simple_hash_table();
+    
+    experiment1();
     return 0;
 }
 
